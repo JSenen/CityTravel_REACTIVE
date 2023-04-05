e29228614874cac34070455c3296b3c1833e0709
Juan Senen <juansenen@hotmail.com>
2023-04-05 18:05:55 +0200

    Repository FLUX and MONO

--- src/main/java/com/juansenen/citytravelREACTIVE/repository/TrainStatusRepository.java
+++ src/main/java/com/juansenen/citytravelREACTIVE/repository/TrainStatusRepository.java
@@ -1,4 +1,16 @@
 package com.juansenen.citytravelREACTIVE.repository;
 
-public interface TrainStatusRepository {
+import com.juansenen.citytravelREACTIVE.domain.TrainStatus;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface TrainStatusRepository extends ReactiveMongoRepository<TrainStatus,String> {
+    /** Lista de elementos FLUX */
+    Flux<TrainStatus> findAll();
+    /** Elemento individual MONO */
+    Mono<TrainStatus> findByCode(String code);
+
 }
