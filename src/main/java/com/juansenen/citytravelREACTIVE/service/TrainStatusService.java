b04346331fe3e80ecad68b889e672ada2e40b4b3
Juan Senen <juansenen@hotmail.com>
2023-04-05 18:09:59 +0200

    Service BASIC

--- src/main/java/com/juansenen/citytravelREACTIVE/service/TrainStatusService.java
+++ src/main/java/com/juansenen/citytravelREACTIVE/service/TrainStatusService.java
@@ -1,4 +1,11 @@
 package com.juansenen.citytravelREACTIVE.service;
 
+import com.juansenen.citytravelREACTIVE.domain.TrainStatus;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface TrainStatusService {
+
+    Flux<TrainStatus> findAll();
+    Mono<TrainStatus> findByCode(String code);
 }
--- src/main/java/com/juansenen/citytravelREACTIVE/service/TrainStatusServiceImpl.java
+++ src/main/java/com/juansenen/citytravelREACTIVE/service/TrainStatusServiceImpl.java
@@ -1,4 +1,22 @@
 package com.juansenen.citytravelREACTIVE.service;
 
-public class TrainStatusServiceImpl {
+import com.juansenen.citytravelREACTIVE.domain.TrainStatus;
+import com.juansenen.citytravelREACTIVE.repository.TrainStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class TrainStatusServiceImpl implements TrainStatusService{
+
+    @Autowired
+    private TrainStatusRepository trainStatusRepository;
+    @Override
+    public Flux<TrainStatus> findAll() {
+        return trainStatusRepository.findAll();
+    }
+
+    @Override
+    public Mono<TrainStatus> findByCode(String code) {
+        return trainStatusRepository.findByCode(code);
+    }
 }
